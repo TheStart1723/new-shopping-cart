8a28f5a9a2e8e328f2945133dae0776657ef944e
TheStart1723 <791058145@qq.com>
2020-01-21 20:49:55 -0600

    static state display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import Sidebar from "react-sidebar";
 import 'rbx/index.css';
-import { Card, Column, Image, Content, Title, Divider, Button } from 'rbx';
+import {
+  Card,
+  Column,
+  Image,
+  Content,
+  Title,
+  Divider,
+  Button,
+  Container,
+  Navbar,
+  Field,
+  Control,
+  Hero
+} from 'rbx';
 
 const sizes = ['S', 'M', 'L', 'XL']
 
@@ -18,7 +32,7 @@ const Cards = ({ product }) => {
             <Title size={6}>
               {product.title}
             </Title>
-            <Content as="p" style={{marginTop: "-15px"}}>{`Description: ${product.description || "N/A"}`}</Content>
+            <Content as="p" style={{ marginTop: "-15px" }}>{`Description: ${product.description || "N/A"}`}</Content>
             <Button.Group style={{ justifyContent: "center", marginTop: "15px" }}>
               {sizes.map(size => (
                 <Button key={size}>{size}</Button>
@@ -36,9 +50,62 @@ const Cards = ({ product }) => {
   )
 }
 
+const NavigationBar = ({ setOpenSidebar }) => {
+  return (
+    <Navbar color="dark" fixed="top">
+      <Navbar.Menu>
+        <Navbar.Segment align="end">
+          <Navbar.Item as="div">
+            <Field kind="group">
+              <Control>
+                <Button color="light" onClick={() => setOpenSidebar(true)}>
+                  <span>Shopping Cart</span>
+                </Button>
+              </Control>
+            </Field>
+          </Navbar.Item>
+        </Navbar.Segment>
+      </Navbar.Menu>
+    </Navbar>
+  )
+}
+
+const SideBar = ({ openSidebar, setOpenSidebar }) => {
+  return (
+    <Sidebar
+      sidebar={
+        <Container>
+          <Hero>
+            <Hero.Body>
+              <Container>
+                <Title align="center" style={{color: "white"}}>Cart</Title>
+              </Container>
+            </Hero.Body>
+          </Hero>
+        </Container>
+      }
+      open={openSidebar}
+      onSetOpen={setOpenSidebar}
+      styles={{
+        sidebar: {
+          transition: "left .1s, right .1s",
+          WebkitTransition: "-webkit-transform .1s ease-out",
+          paddingTop: "53px",
+          width: "300px",
+          background: "gray",
+          position: "fixed"
+        }
+      }}
+      pullRight
+    ></Sidebar>
+  )
+}
+
 const App = () => {
   const [data, setData] = useState({});
+  const [openSidebar, setOpenSidebar] = useState(false);
   const products = Object.values(data);
+
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -49,18 +116,24 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      {[...Array(Math.ceil(products.length / 4)).keys()]
-        .map(i => (products.slice(4 * i, 4 * i + 4)))
-        .map((products, idx) => (
-          <Column.Group key={idx}>
-            {products.map(product => (
-              <Cards product={product} />
-            ))}
-          </Column.Group>
-        ))
-      }
-    </div>
+    <React.Fragment>
+      <NavigationBar setOpenSidebar={setOpenSidebar} />
+      <Container fluid style={{ margin: 0, marginTop: "-20px" }}>
+        <SideBar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
+        <Container style={{ marginTop: "20px" }}>
+          {[...Array(Math.ceil(products.length / 4)).keys()]
+            .map(i => (products.slice(4 * i, 4 * i + 4)))
+            .map((products, idx) => (
+              <Column.Group key={idx}>
+                {products.map(product => (
+                  <Cards product={product} />
+                ))}
+              </Column.Group>
+            ))
+          }
+        </Container>
+      </Container>
+    </React.Fragment>
   );
 };
 
